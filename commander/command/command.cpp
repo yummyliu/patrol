281c0e57c44f8878bffc922a1398cd35089b646c
liuyangming <sdwhlym@gmail.com>
2018-08-06 19:23:11 +0800

    cpprestcompiledone

--- commander/command/command.cpp
+++ commander/command/command.cpp
@@ -10,81 +10,32 @@
 ============================================================================= */
 
 extern "C" {
-	#include <postgres.h>    //all C headers and macros go inside extern "C"
-	#include <utils/rel.h>
-	#include <curl/curl.h>
+#include <postgres.h>    //all C headers and macros go inside extern "C"
+#include <utils/rel.h>
+#include <curl/curl.h>
 
-	#include "command.h"
-	PG_FUNCTION_INFO_V1(getdbinfo);
+#include "command.h"
+
+
+PG_FUNCTION_INFO_V1(getdbinfo);
 }
 
-#include <vector>
 #include <string>
 #include <sstream>
+#include <strstream>
+using namespace std;
+
+
+string RequestJSONValueAsync(string url, string key);
+
 
 extern "C" {
-
-	static std::string DownloadedResponse;
-
-	static int writer(char *data, size_t size, size_t nmemb, std::string *buffer_in)
-	{
-
-	    // Is there anything in the buffer?
-	    if (buffer_in != NULL)
-	    {
-	        // Append the data to the buffer
-	        buffer_in->append(data, size * nmemb);
-
-	        // How much did we write?
-	        DownloadedResponse = *buffer_in;
-
-	        return size * nmemb;
-	    }
-
-	    return 0;
-
-	}
-
-	std::string DownloadJSON(std::string URL)
-	{
-	    CURL *curl;
-	    CURLcode res;
-	    struct curl_slist *headers=NULL; // init to NULL is important
-	    headers = curl_slist_append(headers, "Accept: application/json");
-	    headers = curl_slist_append(headers, "Content-Type: application/json");
-	    headers = curl_slist_append(headers, "charsets: utf-8");
-	    curl = curl_easy_init();
-
-	    if (curl)
-	    {
-			elog(LOG, "url-: %s", URL.c_str());
-	        curl_easy_setopt(curl, CURLOPT_HTTPHEADER, headers);
-	        curl_easy_setopt(curl, CURLOPT_URL, URL.c_str());
-	        curl_easy_setopt(curl, CURLOPT_HTTPGET,1);
-	        curl_easy_setopt(curl,CURLOPT_WRITEFUNCTION,writer);
-	        res = curl_easy_perform(curl);
-
-	        if (CURLE_OK == res)
-	        {
-	            char *ct;
-	            res = curl_easy_getinfo(curl, CURLINFO_CONTENT_TYPE, &ct);
-	            if((CURLE_OK == res) && ct)
-	                return DownloadedResponse;
-	        }
-	    }
-
-	    curl_slist_free_all(headers);
-	}
-
-    Datum
-	getdbinfo(PG_FUNCTION_ARGS) {
-	    std::ostringstream oss;
-
+	Datum getdbinfo(PG_FUNCTION_ARGS) {
+		std::ostringstream oss;
 		auto dbId = PG_GETARG_INT32(0);
-		std::string url("http://localhost:3000/dbinfo/"+std::to_string(dbId));
-
-		std::string res = DownloadJSON(url);
-
-		PG_RETURN_CSTRING(res.c_str());
+		// std::string url("http://localhost:3000/dbinfo/"+std::to_string(dbId));
+		auto url = "http://localhost:3000/dbs";
+		auto ss = RequestJSONValueAsync(url,"dbinfo");
+		PG_RETURN_CSTRING(ss.c_str());
 	}
 }
---
+++ commander/command/restapi.cpp
@@ -0,0 +1,56 @@
+/* =============================================================================
+#     FileName: command.cpp
+#         Desc: command function in commander write in cpp
+#       Author: LiuYangming
+#        Email: sdwhlym@126.com
+#     HomePage: http://yummyliu.github.io
+#      Version: 0.0.1
+#   LastChange: 2018-08-01 15:28:45
+#      History:
+============================================================================= */
+
+#include <string>
+#include <sstream>
+#include <strstream>
+#include <cpprest/http_client.h>
+
+
+using namespace std;
+using namespace utility;
+using namespace web::http;
+using namespace web;
+using namespace web::http::client;
+using namespace concurrency::streams;
+
+
+ostringstream ss;
+
+string RequestJSONValueAsync(string url, string key)
+{
+
+	http_client client(url);
+	client.request(methods::GET)
+		.then([](http_response response) -> pplx::task<json::value>
+				{
+				if(response.status_code() == status_codes::OK)
+				{
+				return response.extract_json();
+				}
+
+				// Handle error cases, for now return empty json value...
+				return pplx::task_from_result(json::value());
+				})
+	.then([&key](pplx::task<json::value> previousTask){
+			try
+			{
+			const json::value& v = previousTask.get();
+			ss << v.as_object().at(key).serialize() <<endl;
+			}
+			catch (const http_exception& e)
+			{
+			// Print error.
+			ss << e.what();
+			}
+			}).wait();
+	return ss.str().c_str();
+}
